36bec2e0a2d2cd17a72726ee513e73498c5755fc
Emeka <G00405014@atu.ie>
2023-10-03 16:10:04 +0100

    Exercise 1 Fixed error

--- src/main/java/ie/atu/demystifyingrequest/RequestController.java
+++ src/main/java/ie/atu/demystifyingrequest/RequestController.java
@@ -1,13 +1,12 @@
 package ie.atu.demystifyingrequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
+        @RestController
 public class RequestController {
 
-    @GetMapping("Hello")
+    @GetMapping("/Hello")
     public String hello(){
         return "Hello, World";
 
