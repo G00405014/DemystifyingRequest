32d0fd2ea27087a6e174a3842eda9b8ac5aa2bb7
Emeka <G00405014@atu.ie>
2023-10-03 16:03:22 +0100

    Exercise 1

--- src/main/java/ie/atu/demystifyingrequest/RequestController.java
+++ src/main/java/ie/atu/demystifyingrequest/RequestController.java
@@ -1,4 +1,16 @@
 package ie.atu.demystifyingrequest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 public class RequestController {
+
+    @GetMapping("Hello")
+    public String hello(){
+        return "Hello, World";
+
+    }
 }
+
